275f812008933cef4e46816c6050167fbe4e9ee1
Ben Tagtow <bentagtow@gmail.com>
2019-11-15 18:07:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,11 +14,11 @@ def get_japanese_emoticon(library, emoticon)
   # code goes here	  dictionary = load_library(library)
   return "Sorry, that emoticon was not found" if dictionary["get_emoticon"][emoticon].nil?
   dictionary["get_emoticon"][emoticon]
-end	end
+end	
 
 
 def get_english_meaning	def get_english_meaning(library, emoticon)
   # code goes here	  dictionary = load_library(library)
   return "Sorry, that emoticon was not found" if dictionary["get_meaning"][emoticon].nil?
   dictionary["get_meaning"][emoticon]
-end 	end 
+end 	
