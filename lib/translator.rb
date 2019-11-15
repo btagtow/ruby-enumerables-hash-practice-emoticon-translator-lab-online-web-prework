1b76e9133f79c3e8cbf7db0792b7f7afc929f6cf
Ben Tagtow <bentagtow@gmail.com>
2019-11-15 18:05:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,8 @@ def load_library(filetype)
     get_meaning[data[1]] = desc 
     get_emoticon[data[0]] = data[1]
   end 
-  tranlations = {"get_meaning" => get_meaning, "get_emoticon"
+  tranlations = {"get_meaning" => get_meaning, "get_emoticon" => get_emoticon} 
+  translations
   # code goes here
 end
 
